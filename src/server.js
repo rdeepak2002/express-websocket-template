44d6778a9bbdc203b4e39125506d0f7f290a9acf
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-09-16 15:57:30 -0400

    Add web socket hello world code

--- src/server.js
+++ src/server.js
@@ -1,12 +1,32 @@
 import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
 
 const app = express();
-const port = 8080;
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const port = process.env.port || 8080;
 
 app.get('/', (req, res) => {
   res.json({ hello: 'world' });
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('User connected to web socket: ', socket);
+});
+
+server.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
